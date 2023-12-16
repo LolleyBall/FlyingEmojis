e0c3181056acad1b15854776056b44a57dabe887
lolleyball <mxicano933@gmail.com>
2023-12-16 14:27:32 +0300

    Added ParticleAnimationView

--- FlyingEmojis/FlyingEmojis.h
+++ FlyingEmojis/FlyingEmojis.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double FlyingEmojisVersionNumber;
 FOUNDATION_EXPORT const unsigned char FlyingEmojisVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FlyingEmojis/PublicHeader.h>
-
-
---
+++ FlyingEmojis/ParticleAnimationView/ParticleAnimationView.swift
@@ -0,0 +1,94 @@
+import UIKit
+
+public class ParticleAnimationView: UIView {
+    private let particleEmitter = CAEmitterLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    func update(with image: UIImage) {
+        let near = makeEmmiterCell(image: image, velocity: 300, scale: 0.7, longitude: .pi)
+        let middle = makeEmmiterCell(image: image, velocity: 200, scale: 0.5)
+        let far = makeEmmiterCell(image: image, velocity: 100, scale: 0.3)
+        particleEmitter.emitterCells = [far, middle, near]
+    }
+
+    func update(with theme: Theme) {
+        let near = makeEmmiterCell(image: theme.mainImage, velocity: 300, scale: 0.7, longitude: .pi)
+        let middle = makeEmmiterCell(image: theme.secondaryImage, velocity: 200, scale: 0.5)
+        let far = makeEmmiterCell(image: theme.tertiaryImage, velocity: 100, scale: 0.3)
+        particleEmitter.emitterCells = [far, middle, near]
+    }
+
+    func updateBounds() {
+        particleEmitter.emitterPosition = CGPoint(x: bounds.maxX, y: -100)
+        particleEmitter.emitterShape = .line
+        particleEmitter.emitterSize = CGSize(width: bounds.size.height, height: 1)
+        particleEmitter.renderMode = .backToFront
+        particleEmitter.birthRate = 1
+    }
+
+    func lavaLampAnimation() {
+        let firstLave = "🔥".toImage()
+        let secondLava = "🩸".toImage()
+        let thirdLava = "💛".toImage()
+        particleEmitter.emitterPosition = CGPoint(x: bounds.midX, y: 100)
+        particleEmitter.emitterShape = .line
+        particleEmitter.emitterSize = CGSize(width: bounds.size.height, height: 1)
+        particleEmitter.renderMode = .backToFront
+
+        let near = makeEmmiterCell(image: firstLave, velocity: 200, scale: 1.5, longitude: .pi)
+        let middle = makeEmmiterCell(image: secondLava, velocity: 150, scale: 1, longitude: .pi)
+        let far = makeEmmiterCell(image: thirdLava, velocity: 100, scale: 1, longitude: .pi)
+        particleEmitter.emitterCells = [far, middle, near]
+        
+        let blurEffect = UIBlurEffect(style: .regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        addSubview(blurEffectView)
+    }
+
+    private func setupUI() {
+        particleEmitter.emitterPosition = CGPoint(x: bounds.maxX, y: -100)
+        particleEmitter.emitterShape = .line
+        particleEmitter.emitterSize = CGSize(width: bounds.size.height, height: 1)
+        particleEmitter.renderMode = .backToFront
+        particleEmitter.birthRate = 1
+        
+        layer.addSublayer(particleEmitter)
+    }
+
+    private func makeEmmiterCell(image: UIImage,
+                                 velocity: CGFloat,
+                                 scale: CGFloat,
+                                 longitude: CGFloat = 10)-> CAEmitterCell {
+        let cell = CAEmitterCell()
+        cell.birthRate = 1
+        cell.lifetime = 15.0
+        cell.velocity = velocity
+        cell.velocityRange = velocity / 2
+        cell.spinRange = 2
+        cell.scale = scale
+        cell.scaleRange = scale / 3
+        cell.emissionLongitude = longitude
+        cell.contents = image.cgImage
+        return cell
+    }
+}
+
+extension ParticleAnimationView {
+    struct Theme {
+        let mainImage: UIImage
+        let secondaryImage: UIImage
+        let tertiaryImage: UIImage
+    }
+}
