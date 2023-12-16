f74b7201ecf5745bba9c0602ef04908e6875f14e
lolleyball <mxicano933@gmail.com>
2023-12-16 14:08:02 +0300

    Project created

---
+++ FlyingEmojis/FlyingEmojis.h
@@ -0,0 +1,18 @@
+//
+//  FlyingEmojis.h
+//  FlyingEmojis
+//
+//  Created by Gleb Sysoev on 16.12.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlyingEmojis.
+FOUNDATION_EXPORT double FlyingEmojisVersionNumber;
+
+//! Project version string for FlyingEmojis.
+FOUNDATION_EXPORT const unsigned char FlyingEmojisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlyingEmojis/PublicHeader.h>
+
+
