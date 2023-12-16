904d9f7f7d2f4df5a6220c490303206518ccb564
lolleyball <mxicano933@gmail.com>
2023-12-16 14:27:19 +0300

    Added extensions

---
+++ FlyingEmojis/Extensions/String+toImage.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension String {
+    func toImage() -> UIImage {
+        let size = CGSize(width: 70, height: 70)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        UIColor.clear.set()
+        let rect = CGRect(origin: .zero, size: size)
+        UIRectFill(CGRect(origin: .zero, size: size))
+        (self as AnyObject).draw(in: rect, withAttributes: [.font: UIFont.systemFont(ofSize: 60)])
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image ?? UIImage()
+    }
+}
---
+++ FlyingEmojis/Extensions/UIImage+averageColor.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+extension UIImage {
+    var averageColor: UIColor? {
+        guard let inputImage = CIImage(image: self) else { return nil }
+        let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
+
+        guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else {
+            return nil }
+        guard let outputImage = filter.outputImage else {
+            return nil }
+
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext(options: [.workingColorSpace: kCFNull])
+        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
+        let color = UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: 1.0)
+        
+        var hue: CGFloat = 0.0
+        var saturation: CGFloat = 0.0
+        var brightness: CGFloat = 0.0
+        color.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: nil)
+
+        return UIColor(hue: hue, saturation: saturation, brightness: max(1.0, brightness * 2), alpha: 1.0)
+    }
+}
